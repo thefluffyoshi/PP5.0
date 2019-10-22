8316fe67c9747ef8ad895cf70f3b875dc54ed502
thefluffyoshi <katielynntrinh@gmail.com>
2019-10-22 13:59:22 -0700

    in progress

--- src/Dog.java
+++ src/Dog.java
@@ -22,4 +22,63 @@ public class Dog
         age = newAge;
         pounds = newPounds;
     }//end multi-arg constructor
+
+
+    //Getters
+    public String getName()
+    {
+        return name;
+    }//end Name getter
+
+    public String getBreed()
+    {
+        return breed;
+    }//end Breed getter
+
+    public int getAge()
+    {
+        return age;
+    }//end Age getter
+
+    public double getPounds()
+    {
+        return pounds;
+    }//end Pound getter
+
+
+    //Setters
+    public void setName(String newName)
+    {
+        name = newName;
+    }//end Name setter
+
+    public void setBreed(String newBreed)
+    {
+        breed = newBreed;
+    }//end Breed setter
+
+    public void setAge(int newAge)
+    {
+        age = newAge;
+    }//end Age setter
+
+    public void setPounds(double newPounds)
+    {
+        pounds = newPounds;
+    }//end Pounds setter
+
+
+    //Brain method
+
+
+    //toString
+    public String toString()
+    {
+        String output = "";
+        output += "Name: " + name;
+        output += "Breed: " + breed;
+        output += "Age: " + age;
+        output += "Pounds: " + pounds;
+        return output;
+    }//end toString
 }//end class Dog
