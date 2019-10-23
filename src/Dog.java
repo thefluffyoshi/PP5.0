dd6885ca5a0fbc76bca1e7e6426cf995769926ce
thefluffyoshi <katielynntrinh@gmail.com>
2019-10-23 11:48:53 -0700

    in progress

--- src/Dog.java
+++ src/Dog.java
@@ -4,7 +4,8 @@ public class Dog
     String name;
     String breed;
     int age;
-    double pounds;
+    int pounds;
+    static int count = 0;
 
     //Constructors
     public Dog()
@@ -12,15 +13,17 @@ public class Dog
         name = null;
         breed = null;
         age = 0;
-        pounds = 0.0;
+        pounds = 0;
+        count++;
     }//default constructor
 
-    public Dog(String newName, String newBreed, int newAge, double newPounds)
+    public Dog(String newName, int newAge, String newBreed, int newPounds)
     {
-        name = newName;
-        breed = newBreed;
-        age = newAge;
-        pounds = newPounds;
+        this.name = newName;
+        this.breed = newBreed;
+        this.age = newAge;
+        this.pounds = newPounds;
+        count++;
     }//end multi-arg constructor
 
 
@@ -40,10 +43,15 @@ public class Dog
         return age;
     }//end Age getter
 
-    public double getPounds()
+    public int getPounds()
     {
         return pounds;
     }//end Pound getter
+
+    public static int getCount()
+    {
+        return count;
+    }//end dog count
 
 
     //Setters
@@ -62,13 +70,18 @@ public class Dog
         age = newAge;
     }//end Age setter
 
-    public void setPounds(double newPounds)
+    public void setPounds(int newPounds)
     {
         pounds = newPounds;
     }//end Pounds setter
 
 
     //Brain method
+    public String toPoundsKilo()
+    {
+        double kilo = (pounds * 0.45359237);
+        return kilo + " kilo";
+    }//end conversion pounds to kilo
 
 
     //toString
@@ -76,8 +89,8 @@ public class Dog
     {
         String output = "";
         output += "Name: " + name;
-        output += "Breed: " + breed;
         output += "Age: " + age;
+        output += "Breed: " + breed;
         output += "Pounds: " + pounds;
         return output;
     }//end toString
---
+++ src/DogDriver.java
@@ -0,0 +1,18 @@
+public class DogDriver
+{
+    public static void main (String args[])
+    {
+        Dog dog1 = new Dog("Fang", 5, "Husky", 80);
+        Dog dog2 = new Dog("Nutmeg", 12, "Australian Sheepdog", 45);
+        Dog dog3 = new Dog("Fluffy", 2, "Corgi", 50);
+
+        //testing getters
+        System.out.println("Fang is a "+ dog1.getBreed() + " and is " + dog1.getAge() + " years old.");
+        System.out.println("");
+        System.out.println("Nutmeg is " + dog2.getPounds() + " lbs.");
+        System.out.println("");
+        //System.out.println();
+        //System.out.println("");
+        System.out.println("There are " + Dog.getCount() + " dogs in this list.");
+    }//end main
+}//end class DogDriver
